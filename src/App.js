4a4779bab3ea44556387bc7d9ef249354e7e08e2
muhammadkashaf <kashafahmed72@gmail.com>
2020-06-28 23:48:42 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import NavBar from './components/Navbar'
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, TextField, Button, CircularProgress } from '@material-ui/core';
+import BarChart from './components/barCharts';
+import PieChart from './components/pieChart';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: 400,
+    margin: '0 auto',
+    marginTop: 50,
+    textAlign: 'center',
+    padding: 15
+  },
+  input: {
+    width: '95%',
+    marginBottom: 10
+  }
+}));
+
+
+function App() {
+
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
+      {/* <NavBar />
+      <Paper elevation={3} className={classes.paper}>
+        <h1>Login</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField label="Enter Email" className={classes.input}/>
+          <TextField label="Enter Password" className={classes.input}/>
+          <Button variant="contained" color="primary" className={classes.input}>
+            Login
+          </Button>
+        </form>
+        <CircularProgress value={50} />
+      </Paper> */}
+
+      <BarChart />
+      <PieChart />
+    </div>
+  );
+}
+
+export default App;
